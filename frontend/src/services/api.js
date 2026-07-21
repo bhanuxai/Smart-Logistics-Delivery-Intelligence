adeb3de70597fa6bfb48369f7851203f73549d9a
unknown <bhanuseshasai95@gmail.com>
2026-07-21 22:09:11 +0530

    updated API calls

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 
 console.log("VITE_API_URL =", import.meta.env.VITE_API_URL);
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'https://smart-logistics-backend-mefz.onrender.com/api';
 
 console.log("API_BASE_URL =", API_BASE_URL);
 
